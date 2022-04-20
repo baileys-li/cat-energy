97e742a22f8315bc28c0c5274e6b5762776da898
Arthur Baileys Li <arthurlitovko@gmail.com>
2022-04-20 21:57:58 +0300

    Add footer

--- gulp/icons.js
+++ gulp/icons.js
@@ -4,14 +4,19 @@ import svgstore from "gulp-svgstore";
 import Path from "./_const.js";
 import { readdirSync } from "fs";
 
-export default function icons() {
+export default function icons(done) {
 	const folders = readdirSync("source/icons");
-	return gulp
-		.src(folders.map((folder) => `source/icons/${folder}/*.svg`))
-		.pipe(
-			svgstore({
-				inlineSvg: true,
-			})
-		)
-		.pipe(gulp.dest(Path.ICON.build));
+
+	folders.forEach((folder) =>
+		gulp
+			.src(`source/icons/${folder}/*.svg`)
+			.pipe(
+				svgstore({
+					inlineSvg: true,
+				})
+			)
+			.pipe(gulp.dest(Path.ICON.build))
+	);
+
+	done();
 }
