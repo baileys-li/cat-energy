70b7cdac346198b1e61c0bb0c8c109109f93bb36
Arthur Baileys Li <arthurlitovko@gmail.com>
2022-05-04 18:23:19 +0300

    Demo intro block

--- source/js/navigation.js
+++ source/js/navigation.js
@@ -3,7 +3,9 @@ navButton.hidden = false;
 navButton.addEventListener("click", toggleNavigation);
 
 function toggleNavigation() {
-	const willBeOpened = navButton.ariaExpanded === "false";
+	const willBeOpened = navButton.ariaExpanded === "false",
+		message = willBeOpened ? "Закрыть навигацию" : "Открыть навигацию";
 	navButton.ariaExpanded = String(willBeOpened);
-	navButton.ariaLabel = willBeOpened ? "Закрыть навигацию" : "Открыть навигацию"
+	navButton.ariaLabel = message;
+	navButton.title = message;
 }
